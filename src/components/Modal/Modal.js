d5d7dfc8b82724cf83c2c947b7908949d2a5539c
johnvenkiah <contact@johnvenkiah.com>
2023-03-22 00:47:47 +0100

    create styled-component used in Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,4 +1,4 @@
-import { IntroModal } from './Modal.styled';
+import { IntroModal, ModalContainer } from './Modal.styled';
 import { useEffect, useRef } from 'react';
 import { motion } from 'framer-motion';
 import { AnimatePresence } from 'framer-motion';
@@ -41,16 +41,15 @@ const Modal = ({ isOpen, toggleModal, closeOnOutsideClick, children }) => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
-        transition={{ duration: 0.3, ease: 'easeInOut' }}
         key={isOpen}
       >
         <IntroModal>
-          <div ref={modalRef}>
+          <ModalContainer ref={modalRef}>
             <button onClick={toggleModal}>
               <IoMdClose style={closeButtonStyle} />
             </button>
             {children}
-          </div>
+          </ModalContainer>
         </IntroModal>
       </motion.div>
       )
--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -10,60 +10,59 @@ export const IntroModal = styled.div`
   top: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.4);
-  backdrop-filter: blur(10px);
+  background-color: rgba(0, 0, 0, 0.7);
 
-  div:nth-child(1) {
+  button {
     position: absolute;
-    top: 50%;
-    width: 30%;
-    background-color: rgba(38, 50, 50, 0.5);
-    text-align: center;
-    transform: translateY(-50%);
-    padding: 1rem 0.2rem 0.5rem;
-    border-top: 0.7px solid #b6b6b666;
-    border-right: 0.7px solid #8e8e8e66;
-    border-bottom: 0.7px solid #000;
-    border-left: 0.7px solid #77777766;
-    border-radius: 18px;
-    box-shadow: 0 0 1rem 0 #000;
-    min-height: 200px;
-    display: inherit;
-    flex-wrap: wrap;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    transition: all 0.3s ease-in-out;
-
-    @media screen and (max-width: 768px) {
-      width: 80vw;
-      height: 80vh;
+    top: 0.3rem;
+    right: 0.3rem;
+    padding: 0.5rem;
+    border: none;
+    background: transparent;
+    transition: transform 0.2s ease-in-out;
+    &:hover {
+      transform: scale(1.1);
     }
+  }
 
-    button {
-      position: absolute;
-      top: 0.3rem;
-      right: 0.3rem;
-      padding: 0.5rem;
-      border: none;
-      background: transparent;
-      transition: transform 0.2s ease-in-out;
-      &:hover {
-        transform: scale(1.1);
-      }
-    }
+  img {
+    width: 50%;
+    box-shadow: 0 0 15px #000;
+    margin-top: 1rem;
+  }
 
-    img {
-      width: 50%;
-      box-shadow: 0 0 15px #000;
-      margin-top: 1rem;
-    }
+  p {
+    font-weight: bold;
+    font-size: 1rem;
+    padding: 0 1rem;
+  }
+`;
 
-    p {
-      font-weight: bold;
-      font-size: 1rem;
-      padding: 0 1rem;
-    }
+export const ModalContainer = styled.div`
+  backdrop-filter: blur(10px);
+  position: absolute;
+  top: 50%;
+  width: 30%;
+  background-color: rgba(38, 50, 50, 0.5);
+  text-align: center;
+  transform: translateY(-50%);
+  padding: 1rem 0.2rem 0.5rem;
+  border-top: 0.7px solid #b6b6b666;
+  border-right: 0.7px solid #8e8e8e66;
+  border-bottom: 0.7px solid #000;
+  border-left: 0.7px solid #77777766;
+  border-radius: 18px;
+  box-shadow: 0 0 1rem 0 #000;
+  min-height: 200px;
+  display: inherit;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  @media screen and (max-width: 768px) {
+    width: 80vw;
+    height: 80vh;
   }
 `;
 
@@ -74,6 +73,5 @@ export const ModalWrapper = styled.div`
   align-items: center;
   width: 50%;
   height: 25%;
-  background-color: transparent !important;
   font-size: 2rem;
 `;
