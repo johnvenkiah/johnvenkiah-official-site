1e375b2a574c79190f6159d8d0b4ddccb0db4829
johnvenkiah <contact@johnvenkiah.com>
2023-04-28 00:16:43 +0200

    connecting to backend correctly

---
+++ server/server.js
@@ -0,0 +1,14 @@
+// const express = require('express');
+// const cors = require('cors');
+
+// const app = express();
+
+// app.use(cors());
+
+// app.get('/api', (req, res) => {
+//   res.json({ users: ['userOne', 'userTwo', 'userThree'] });
+// });
+
+// app.listen(5001, () => {
+//   console.log('Server is running on port 5001');
+// });
--- src/pages/live.js
+++ src/pages/live.js
@@ -1,41 +1,110 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import moment from 'moment';
+import momentDurationFormatSetup from 'moment-duration-format';
+momentDurationFormatSetup(moment);
 
 export default function Live() {
+  const [calState, setCalState] = useState({
+    time: moment().format('dd, Do MMMM, h:mm A'),
+    events: [],
+    isEmpty: false,
+    isLoading: true,
+  });
+
+  useEffect(() => {
+    // const startPolling = () => {
+    //   setTimeout(() => {
+    //     getEvents();
+    //     startPolling();
+    //   }, 60000);
+    // };
+
+    const getEvents = () => {
+      fetch('http://localhost:3000/events')
+        .then((response) => response.json())
+        .then((data) => {
+          let events = data;
+          if (events.length > 0) {
+            setCalState({
+              events: events,
+              isLoading: false,
+              isEmpty: false,
+            });
+          } else {
+            setCalState({
+              isEmpty: true,
+              isLoading: false,
+            });
+          }
+        })
+        .catch((error) => {
+          console.error('This is the error: ', error);
+        });
+    };
+    getEvents();
+  }, []);
+
+  const { events } = calState;
+  let eventsList = events.map(function (event) {
+    if (event.description) {
+      let descr = event.description;
+      descr = descr.slice(descr.lastIndexOf('https:'));
+      descr = descr.replace('</u>', '');
+      descr = descr.replace('</a>', '');
+
+      return (
+        <a
+          className="list-group-item"
+          href={descr}
+          target="_blank"
+          rel="noreferrer"
+          key={event.id}
+        >
+          <span className="date">
+            {moment(event.start.dateTime).format('D/M-Y')}
+            {' - '}
+          </span>
+          <span className="location">{event.location}</span>
+        </a>
+      );
+    } else {
+      return (
+        <p className="list-group-item" key={event.id}>
+          <span className="date">
+            {moment(event.start.dateTime).format('D/M-Y')}
+            {' - '}
+          </span>
+          <span className="location">{event.location}</span>
+        </p>
+      );
+    }
+  });
+
+  let emptyState = (
+    <div className="empty">
+      <h3>No gigs currently in our calendar.</h3>
+      <p>
+        We are updating it continuously, so it may currently be under
+        maintenance. Please contact us if you have any queries!
+      </p>
+    </div>
+  );
+
+  let loadingState = (
+    <div className="loading">
+      <h4>Loading dates...</h4>
+    </div>
+  );
+
   return (
-    <div>
-      <p>This is where you'll see all my wonderful gigs.</p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
-        tincidunt nisl, eu aliquam nisl. Nulla facilisi. Nulla facilisi. Nulla
-        facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-        facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-        facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-        facilisi. Nulla facilisi.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
-        tincidunt nisl, eu aliquam nisl. Nulla facilisi. Nulla facilisi. Nulla
-        facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-        facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-        facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-        facilisi. Nulla facilisi.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
-        tincidunt nisl, eu aliquam nisl. Nulla facilisi. Nulla facilisi. Nulla
-        facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-        facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-        facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-        facilisi. Nulla facilisi.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
-        tincidunt nisl, eu aliquam nisl. Nulla facilisi. Nulla facilisi. Nulla
-        facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-        facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-        facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-        facilisi. Nulla facilisi.
-      </p>
+    <div className="container">
+      <div className="concerts">
+        <div className="list-group">
+          {calState.isLoading && loadingState}
+          {events.length > 0 && eventsList}
+          {calState.isEmpty && emptyState}
+        </div>
+      </div>
     </div>
   );
 }
