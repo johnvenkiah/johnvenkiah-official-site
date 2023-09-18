9083fac336d1d90e5d3009932e23c061c59a7bf4
johnvenkiah <contact@johnvenkiah.com>
2023-09-18 21:55:14 +0200

    remove express from events.js

--- netlify/functions/events.js
+++ netlify/functions/events.js
@@ -1,24 +1,12 @@
-const path = require('path');
-const express = require('express');
 const { google } = require('googleapis');
-const cors = require('cors');
-require('dotenv').config();
 const nodemailer = require('nodemailer');
-
-const app = express();
+require('dotenv').config();
 
 const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
 const GOOGLE_PRIVATE_KEY = process.env.PRIVATE_KEY.replace(/\\n/g, '\n');
 const GOOGLE_CLIENT_EMAIL = process.env.CLIENT_EMAIL;
 const GOOGLE_PROJECT_NUMBER = process.env.PROJECT_NUMBER;
 const GOOGLE_CALENDAR_ID = process.env.CALENDAR_ID;
-const PORT = process.env.PORT || 3030;
-
-var corsOptions = {
-  origin: 'https://johnvenkiah.com',
-  // origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200,
-};
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -28,11 +16,7 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'build')));
-app.use(cors(corsOptions));
-
-module.exports.handler = async (event, context) => {
+exports.handler = async (event, context) => {
   const { httpMethod, queryStringParameters } = event;
 
   if (
@@ -41,36 +25,30 @@ module.exports.handler = async (event, context) => {
     queryStringParameters.endpoint === 'events'
   ) {
     try {
-      app.get('/events', async (req, res) => {
-        // try {
-        const jwtClient = new google.auth.JWT(
-          GOOGLE_CLIENT_EMAIL,
-          null,
-          GOOGLE_PRIVATE_KEY,
-          SCOPES
-        );
+      const jwtClient = new google.auth.JWT(
+        GOOGLE_CLIENT_EMAIL,
+        null,
+        GOOGLE_PRIVATE_KEY,
+        SCOPES
+      );
 
-        const calendar = google.calendar({
-          version: 'v3',
-          project: GOOGLE_PROJECT_NUMBER,
-          auth: jwtClient,
-        });
-        const { data } = await calendar.events.list({
-          calendarId: GOOGLE_CALENDAR_ID,
-          timeMin: new Date().toISOString(),
-          maxResults: 10,
-          fields: 'items(id,summary,description,location,start/dateTime)',
-          singleEvents: true,
-          orderBy: 'startTime',
-        });
-        data.items
-          ? res.json(data.items)
-          : res.json('No upcoming events found.');
-        // } catch (error) {
-        //   console.error(error);
-        //   res.status(500).send('Server Error');
-        // }
+      const calendar = google.calendar({
+        version: 'v3',
+        project: GOOGLE_PROJECT_NUMBER,
+        auth: jwtClient,
       });
+      const { data } = await calendar.events.list({
+        calendarId: GOOGLE_CALENDAR_ID,
+        timeMin: new Date().toISOString(),
+        maxResults: 10,
+        fields: 'items(id,summary,description,location,start/dateTime)',
+        singleEvents: true,
+        orderBy: 'startTime',
+      });
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data.items || 'No upcoming events found.'),
+      };
     } catch (error) {
       console.error(error);
       return {
@@ -86,52 +64,51 @@ module.exports.handler = async (event, context) => {
     queryStringParameters.endpoint === 'send'
   ) {
     try {
-      app.post('/send', (req, res) => {
-        // try {
-        const mailOptions = {
-          from: req.body.email, // sender address
-          to: process.env.EMAIL, // list of receivers
-          subject: `johnvenkiah.com - ${req.body.subject}`, // Subject line
-          html: `
-      <h3>Contact Request From ${req.body.name}</h3>
-      <p><strong>From: </strong>${req.body.name} | ${req.body.email}</p>
-      <p><strong>Subject: </strong>johnvenkiah.com - ${req.body.subject}</li>
-      <h4>Message:</h4>
-      <p>${req.body.message}</p>
-      `,
-        };
+      const requestData = JSON.parse(event.body);
+      const mailOptions = {
+        from: requestData.email, // sender address
+        to: process.env.EMAIL, // list of receivers
+        subject: `johnvenkiah.com - ${requestData.subject}`, // Subject line
+        html: `
+    <h3>Contact Request From ${requestData.name}</h3>
+    <p><strong>From: </strong>${requestData.name} | ${requestData.email}</p>
+    <p><strong>Subject: </strong>johnvenkiah.com - ${requestData.subject}</li>
+    <h4>Message:</h4>
+    <p>${requestData.message}</p>
+    `,
+      };
 
-        transporter.sendMail(mailOptions, function (err, info) {
-          if (err) {
-            console.log(err);
-            res.status(500).send({
+      transporter.sendMail(mailOptions, function (err, info) {
+        if (err) {
+          console.log(err);
+          return {
+            statusCode: 500,
+            body: JSON.stringify({
               success: false,
               message: 'Something went wrong. Try again later',
-            });
-          } else {
-            res.send({
+            }),
+          };
+        } else {
+          return {
+            statusCode: 200,
+            body: JSON.stringify({
               success: true,
               message: "Thanks for writing, I'll get back to you soon!",
-            });
-          }
-        });
-        // } catch (error) {
-        //   res.status(500).send({
-        //     success: false,
-        //     message: error,
-        //   });
-        // }
+            }),
+          };
+        }
       });
     } catch (error) {
       console.error(error);
       return {
         statusCode: 500,
-        body: JSON.stringify({ message: 'Server Error' }),
+        body: JSON.stringify({ message: 'Email sending failed' }),
       };
     }
+  } else {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ message: 'Not Found' }),
+    };
   }
-
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-  });
 };
