6b1f0c61510616e6f339a0ebd77ab153734b4651
johnvenkiah <contact@johnvenkiah.com>
2023-10-01 01:38:39 +0200

    add lighter images, remove heavier ones and redo animation for bg

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,15 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { BackgroundImg } from './Background.styled';
 import { useLocation } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import johnMopaSinging from '../../images/background/mopa_singing_portrait.png';
+import johnMopaSinging from '../../images/background/mopa_singing_portrait.jpg';
 import johnMopaSingingLs from '../../images/background/mopa_singing.jpeg';
 import hands from '../../images/background/hands.jpg';
-import playingRearShirt from '../../images/background/playing_rear_shirt.jpg';
-import soloProfile from '../../images/background/solo_profile.jpg';
-import soloRear from '../../images/background/solo_rear.jpg';
+import playingRearShirt from '../../images/background/playing_rear_shirt.jpeg';
+import soloProfile from '../../images/background/solo_profile.jpeg';
+import soloRear from '../../images/background/solo_rear.jpeg';
 import blackClav from '../../images/background/john_black_clav.png';
-import johnFolkRockPtrt from '../../images/background/john_folk_rock_portrait.png';
+import johnFolkRockPtrt from '../../images/background/john_folk_rock_portrait.jpeg';
 import johnFolkRockLscp from '../../images/background/john_folk_rock.jpeg';
 
 const Background = () => {
@@ -26,19 +26,43 @@ const Background = () => {
 
   const [isLoaded, setIsLoaded] = useState(false);
 
+  const backgroundImages = useMemo(() => {
+    return {
+      '/': johnMopa,
+      '/music': hands,
+      '/videos': playingRearShirt,
+      '/live': johnFolkRock,
+      '/developer': blackClav,
+      '/about': soloRear,
+      '/contact': soloProfile,
+    };
+  }, [johnMopa, johnFolkRock]);
+
   const handleImageLoad = () => {
     setIsLoaded(true);
   };
 
-  const backgroundImages = {
-    '/': johnMopa,
-    '/music': hands,
-    '/videos': playingRearShirt,
-    '/live': johnFolkRock,
-    '/developer': blackClav,
-    '/about': soloRear,
-    '/contact': soloProfile,
-  };
+  useEffect(() => {
+    // Clear isLoaded when pathname changes to trigger a delay when loading a new image
+    setIsLoaded(false);
+
+    const image = new Image();
+    image.src = backgroundImages[pathname];
+    // image.src = `${backgroundImages[pathname]}?${Math.random()}`;
+    image.onload = () => {
+      // Use setTimeout test the loading animation
+      // setTimeout(() => {
+      //   console.log('Setting isLoaded to true');
+      //   setIsLoaded(true);
+      // }, 2000);
+      setIsLoaded(true);
+    };
+
+    return () => {
+      // Clean up the image load event listener if the component unmounts
+      image.onload = null;
+    };
+  }, [pathname, backgroundImages]);
 
   const style = {
     boxShadow: 'inset 0 0 50px #000',
@@ -54,24 +78,21 @@ const Background = () => {
   no-repeat center center/cover`,
   };
 
+  const motionDivProps = {
+    initial: { opacity: 0, scale: 1.01 },
+    animate: isLoaded ? { opacity: 1, scale: 1 } : {},
+    exit: { opacity: 0, scale: 1.1 },
+    transition: { duration: 0.8, ease: [0.73, 0.26, 0.39, 0.82] },
+    key: pathname,
+  };
+
   return (
     <>
-      <motion.div
-        style={style}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.8, ease: [0.17, 0.67, 0.83, 0.67] }}
-        key={pathname}
-      >
+      <motion.div {...motionDivProps} style={style}>
         <BackgroundImg
           style={!isLoaded ? loadingBGStyle : loadedBGStyle}
           key={pathname}
-        />
-        <img
-          src={backgroundImages[pathname]}
           alt="Background"
-          style={{ display: 'none' }}
           onLoad={handleImageLoad}
         />
       </motion.div>
